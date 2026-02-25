58c7fca09592c4112f0b2a71b74c1d4a19b22288
ginogiorgi <ginorubengiorgi@gmail.com>
2026-02-25 03:22:09 +0000

    Proyect initialiced and changes for host in gh-pages

---
+++ pyrux_portfolio/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  output: 'export',
+  
+  // Si tu repositorio se llama "pyrux_portfolio", descomenta la siguiente línea:
+  // basePath: '/pyrux_portfolio',
+  
+  // Si es tu repositorio de usuario (username.github.io), deja basePath comentado
+  
+  images: {
+    unoptimized: true, // Necesario para exportación estática
+  },
+};
+
+export default nextConfig;
